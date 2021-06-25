b668d357c1d9216587b12fb2edabe0c0af471edd
Thomas Nguy <thomas.nguy@crypto.com>
2021-06-25 16:53:43 +0900

    rename ante handler

--- app/ante/eth.go
+++ app/ante/eth.go
@@ -543,7 +543,6 @@ func (vbd EthValidateBasicDecorator) AnteHandle(ctx sdk.Context, tx sdk.Tx, simu
 	return next(ctx, tx, simulate)
 }
 
-
 // EthSetupContextDecorator is adapted from SetUpContextDecorator from cosmos-sdk, it ignores gas consumption
 // by setting the gas meter to infinite
 type EthSetupContextDecorator struct{}
